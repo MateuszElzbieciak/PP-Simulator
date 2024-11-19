14c988bd7772a13971af9286c62a56a7ea814db8
MateuszElzbieciak <matix.elzbi@icloud.com>
2024-11-19 22:00:23 +0100

    xUnit

--- Simulator/obj/Debug/net8.0/Simulator.AssemblyInfo.cs
+++ Simulator/obj/Debug/net8.0/Simulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b3b29cce186a87f59cae99f5b4b0746b0b08a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c08546f7721faf3ddff624bbd81ccbf37bfc168")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
